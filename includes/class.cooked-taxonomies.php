8c5318646ddf16dc476cc3d7ee877db8e79b3891
Armand Tresova <xjsv24@gmail.com>
2024-07-30 19:48:35 -0400

    Enabled REST API for Recipe Categories #41

--- includes/class.cooked-taxonomies.php
+++ includes/class.cooked-taxonomies.php
@@ -20,49 +20,45 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 class Cooked_Taxonomies {
 
 	public static function get() {
-
 		global $query_var;
 
 		$_cooked_settings = Cooked_Settings::get();
 
 		$front_page_id = get_option( 'page_on_front' );
-		if ( $_cooked_settings['browse_page'] == $front_page_id ) {
-			$query_var = false;
-		} else {
-			$query_var = true;
-		}
+		$query_var = ($_cooked_settings['browse_page'] == $front_page_id) ? false : true;
 
-		$taxonomy_permalinks = apply_filters( 'cooked_taxonomy_settings', array(
-			'cp_recipe_category' => ( isset($_cooked_settings['recipe_category_permalink']) && $_cooked_settings['recipe_category_permalink'] ? $_cooked_settings['recipe_category_permalink'] : 'recipe-category' )
-		));
+		$taxonomy_permalinks = apply_filters( 'cooked_taxonomy_settings', [
+			'cp_recipe_category' => (isset($_cooked_settings['recipe_category_permalink']) && $_cooked_settings['recipe_category_permalink'] ? $_cooked_settings['recipe_category_permalink'] : 'recipe-category')
+		]);
 
-		$taxonomies = apply_filters( 'cooked_taxonomies', array(
-			'cp_recipe_category' => array(
-				'hierarchical'        => true,
-				'labels'              => array(
-					'name'                => __('Categories', 'cooked'),
-					'singular_name'       => __('Category', 'cooked'),
-					'search_items'        => __('Search Categories', 'cooked'),
-					'all_items'           => __('All Categories', 'cooked'),
-					'parent_item'         => __('Parent Category', 'cooked'),
-					'parent_item_colon'   => __('Parent Category:', 'cooked'),
-					'edit_item'           => __('Edit Category', 'cooked'),
-					'update_item'         => __('Update Category', 'cooked'),
-					'add_new_item'        => __('Add New Category', 'cooked'),
-					'new_item_name'       => __('New Category Name', 'cooked'),
-					'menu_name'           => __('Categories', 'cooked'),
-					'not_found'           => __('No Categories', 'cooked')
-				),
-				'show_ui'             => true,
-				'show_admin_column'   => true,
-				'show_in_menu'		  => false,
-				'rest_base'             => 'recipe_category',
-    			'rest_controller_class' => 'WP_REST_Terms_Controller',
-				'query_var'           => $query_var,
-				'rewrite'             => array( 'slug' => $taxonomy_permalinks['cp_recipe_category'] )
-			)
+		$taxonomies = apply_filters( 'cooked_taxonomies', [
+			'cp_recipe_category' => [
+				'hierarchical' => true,
+				'labels' => [
+					'name' => __('Categories', 'cooked'),
+					'singular_name' => __('Category', 'cooked'),
+					'search_items' => __('Search Categories', 'cooked'),
+					'all_items' => __('All Categories', 'cooked'),
+					'parent_item' => __('Parent Category', 'cooked'),
+					'parent_item_colon' => __('Parent Category:', 'cooked'),
+					'edit_item' => __('Edit Category', 'cooked'),
+					'update_item' => __('Update Category', 'cooked'),
+					'add_new_item' => __('Add New Category', 'cooked'),
+					'new_item_name' => __('New Category Name', 'cooked'),
+					'menu_name' => __('Categories', 'cooked'),
+					'not_found' => __('No Categories', 'cooked')
+				],
+				'show_ui' => true,
+				'show_admin_column' => true,
+				'show_in_menu' => false,
+				'show_in_rest' => true,
+				'rest_base' => 'recipe_category',
+				'rest_controller_class' => 'WP_REST_Terms_Controller',
+				'query_var' => $query_var,
+				'rewrite' => ['slug' => $taxonomy_permalinks['cp_recipe_category']]
+			]
 
-		), $taxonomy_permalinks, $query_var );
+		], $taxonomy_permalinks, $query_var );
 
 		if ( !in_array( 'cp_recipe_category', $_cooked_settings['recipe_taxonomies'] ) ): unset( $taxonomies['cp_recipe_category'] ); endif;
 

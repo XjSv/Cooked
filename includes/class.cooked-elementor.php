f1d43e253386134746e9ac97b2f45a3c27ae52c6
Armand Tresova <xjsv24@gmail.com>
2026-02-23 15:32:27 -0500

    Fix for #126 Fullscreen not working inside Elementor

--- includes/class.cooked-elementor.php
+++ includes/class.cooked-elementor.php
@@ -26,7 +26,7 @@ class Cooked_Elementor {
     public function init() {
         // Check if Elementor installed and activated
         if ( did_action( 'elementor/loaded' ) ) {
-            add_filter( 'cooked_recipe_content_filter', [&$this, 'elementor_filter'], 15, 3 );
+            add_filter( 'cooked_recipe_content_filter', [&$this, 'elementor_filter'], 15, 4 );
             add_filter( 'cooked_should_update_post_content', [&$this, 'should_update_content'], 10, 2 );
             // Deprecated. Now handled in pre_do_shortcode_tag filter in class.cooked-shortcodes.php.
             // add_action( 'elementor/element/before_section_start', [&$this, 'elementor_is_editing'], 10, 3 );
@@ -49,10 +49,15 @@ class Cooked_Elementor {
         }
     } */
 
-    public function elementor_filter( $recipe_content, $og_content, $recipe_id ) {
+    public function elementor_filter( $recipe_content, $og_content, $recipe_id, $layout_content = null ) {
         $elementor_page = get_post_meta( $recipe_id, '_elementor_edit_mode', true );
 
         if ( $elementor_page ) {
+            $og_content = (string) $og_content;
+            if ( Cooked_Recipes::recipe_has_fullscreen( $recipe_id, $layout_content ) ) {
+                global $recipe_settings;
+                $og_content .= Cooked_Recipes::get_fsm_markup( $recipe_id, $recipe_settings );
+            }
             return $og_content;
         }
 
